af784b14bd2bbdd8278f1e0650105bcfd7b43b45
Satwik <satwikarnav@gmail.com>
2025-01-14 16:21:25 +0530

    th com

--- bakend/index.js
+++ bakend/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chatass.vercel.app",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
